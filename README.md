f0b9d69981957484c4a720e32c27f06c5dddc89e
Henry Peterson <hgpeterson@caltech.edu>
2024-12-12 11:04:46 -0800

    SASE highlight

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Personal Website 
-Feel free to use my template if you like the way it looks.
--- addHeaders.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("title").innerHTML =
-    '<h1>Henry G. Peterson</h1>';
-document.getElementById("navMenu").innerHTML =
-    '<h2><a href="index.html">Home</a> &nbsp; | &nbsp;'+
-    '<a href="publications.html">Publications</a> &nbsp; | &nbsp;'+
-    '<a href="contact.html">Contact</a></h2>';
